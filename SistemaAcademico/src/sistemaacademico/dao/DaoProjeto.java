c3cb6c13fd79fdf76d58adfcc43fac38108d81de
Italo Silva <italo@maleldil.com>
2016-11-23 02:48:14 -0200

    Adiciona método de recuperação dos projetos de um gerente
    Necessita de um campo que ainda não existe no banco.

--- SistemaAcademico/src/sistemaacademico/dao/DaoProjeto.java
+++ SistemaAcademico/src/sistemaacademico/dao/DaoProjeto.java
@@ -98,6 +98,12 @@ public class DaoProjeto {
         }
     }
 
+    // TODO: É necessário introduzir o campo 'gerente_proj' na tabela 'projeto',
+    // que é uma FK para o Colaborador que é gerente do projeto.
+    public static ArrayList<Projeto> recuperarProjetosDoGerente(int codigoGerente) {
+        return recuperarQuery("WHERE gerente_proj = " + codigoGerente);
+    }
+
     public static Projeto recuperar(int codigo) {
         ArrayList<Projeto> result = recuperarQuery("where cod_proj = " + codigo);
         if (!result.isEmpty()) {
